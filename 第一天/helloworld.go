49d1c11894b7cbdb22a1e8706758dc15f9ccd2b1
abbott <abbott68@126.com>
2020-11-03 15:04:54 +0800

    second-commit

--- 第一天/helloworld.go
+++ 第一天/helloworld.go
@@ -1,5 +1,5 @@
-package main
-import "fmt"
-func main()  {
-	fmt.Println("Hello world!")
+package main   // 定义包名
+import "fmt"   // 导入fmt函数  函数包括输入/输出
+func main()  {   //定义程序执行开始的函数
+	fmt.Println("Hello world!") //调用fmt函数里print方法输出
 }
