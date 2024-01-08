2b76af3a02aac90b164608539b94c512d3d8f75b
dev <39670621+abbott68@users.noreply.github.com>
2024-01-08 15:03:31 +0800

    new  变量

---
+++ Chapter 2/Variables/var-ex1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 定义整数类型变量
+	var age int = 30
+	fmt.Println("年龄", age)
+
+	// 定义浮点数类型变量
+	var price float64 = 39.99
+	fmt.Println("价格；", price)
+
+	// 定义字符串类型变量
+	var name string = "john Don"
+	fmt.Println("姓名：", name)
+
+	// 定义布尔类型变量
+	var isStudent bool = true
+	fmt.Println("是否是学生：", isStudent)
+}
