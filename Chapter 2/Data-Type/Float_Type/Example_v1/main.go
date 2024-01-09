5dd2431ebb98d7e03a40678cf2f97f4734f77198
dev <39670621+abbott68@users.noreply.github.com>
2024-01-09 09:02:22 +0800

    new

--- Chapter 2/Data-Type/Float_Type/Example_v1/main.go
+++ Chapter 2/Data-Type/Float_Type/Example_v1/main.go
@@ -5,8 +5,10 @@ import (
 )
 
 func main() {
-	var a float64 = 1.0
-
+	var a float32 = 1.2
+	var b float32 = 2.3
 	fmt.Println(a)
-
+	fmt.Println(b)
+	fmt.Println("========================")
+	fmt.Println(a + b)
 }
